4306395f34b8abb84a3d800dd28c59909e4eaec9
touhami-ben <brooklyn.ben76@gmail.com>
2023-05-19 12:52:53 -0400

    seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,6 @@
 \c cars_dev
 
 INSERT INTO cars (name, image_url, price, make, condition , is_available) VALUES
- ('sienna', 'url', 48.000, 'toyota', 'brand new', true),
-('camry', 'url', 24.000, 'toyota', 'used', true);
+ ('sienna', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIhE9Uy53ysuFP69wiqlJE9qzz-Katc6VLgnmgGZg2vCZCU0cv', 48.000, 'toyota', 'brand new', true),
+('camry', 'https://pictures.dealer.com/h/hudsonhyundai/0763/51e9d78ef7b35469783652fac042317dx.jpg?impolicy=resize&w=1024', 24.000, 'toyota', 'used', true),
+('911-Carrera-S', 'https://files.porsche.com/filestore/image/multimedia/none/992-c2s-modelimage-sideshot/model/fb392e88-9df3-11ec-80eb-005056bbdc38/porsche-model.png', 135.000, 'porsh', 'new', true )
